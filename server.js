665cbf996fb2ceac4c3e6743f44937c95ec62d23
Modou DIOP <modoudiop@gainde2000.sn>
2022-05-10 15:01:59 +0000

    post and get

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    var email = req.body.email;
+    var amount = req.body.amount;
+    res.send({"amount":amount,"email":email});
+})
+
+app.listen(3000,()=>{
+    console.log('server running on port 3000');
+});
